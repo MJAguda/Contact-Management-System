df7574c39f804d7a90762453760715af5bfaa29e
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-05 13:35:03 +0800

    Remove unused imports in ContactController

--- src/main/java/com/mjdminer/springboot/cms/controller/ContactController.java
+++ src/main/java/com/mjdminer/springboot/cms/controller/ContactController.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.mjdminer.springboot.cms.model.Contact;
 import com.mjdminer.springboot.cms.services.ContactService;
 
