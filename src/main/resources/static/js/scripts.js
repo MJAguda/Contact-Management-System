32a903dc7361644492dd79a2a55be905777e7457
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-06 21:50:00 +0800

    Add event listeners to buttons for page title updates

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('addButton').addEventListener('click', function() {
+    document.getElementById('pageTitle').innerText = 'Add Contact';
+});
+
+document.getElementById('updateButton').addEventListener('click', function() {
+    document.getElementById('pageTitle').innerText = 'Update Contact';
+});
+
+document.getElementById('detailsButton').addEventListener('click', function() {
+    document.getElementById('pageTitle').innerText = 'Details';
+});
