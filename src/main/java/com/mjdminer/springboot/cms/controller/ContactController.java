4c051330b0279581c1f8b42095e6736b3d19f389
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-06 10:57:24 +0800

    Add "add-contact" page and controller mapping

--- src/main/java/com/mjdminer/springboot/cms/controller/ContactController.java
+++ src/main/java/com/mjdminer/springboot/cms/controller/ContactController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.mjdminer.springboot.cms.model.Contact;
 import com.mjdminer.springboot.cms.services.ContactService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class ContactController {
@@ -25,6 +28,14 @@ public class ContactController {
         //return to index.html
         return "index";
     }
+
+    // Mapping for localhost:8080/add-contact
+    @GetMapping("/add-contact")
+    public String addContact(ModelMap model){
+        //return to add-contact.html
+        return "add-contact";
+    }
+    
 
     // @RequestMapping("/list-contact")
     // public List<Contact> listAllContacts(ModelMap model){
