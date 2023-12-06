e89636ed36fa414c9f65c249cee466eb6994d6d7
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-06 21:50:33 +0800

    Add new methods to ContactController

--- src/main/java/com/mjdminer/springboot/cms/controller/ContactController.java
+++ src/main/java/com/mjdminer/springboot/cms/controller/ContactController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mjdminer.springboot.cms.model.Contact;
 import com.mjdminer.springboot.cms.services.ContactService;
@@ -20,7 +21,12 @@ public class ContactController {
         this.contactService = contactService;
     }
 
-    // GetMapping for localhost:8080/
+    /**
+     * Retrieves a list of all contacts and adds them to the model.
+     * 
+     * @param model the model to add the contacts to
+     * @return the name of the view to render
+     */
     @GetMapping("/")
     public String listAllContacts(ModelMap model) {
         List<Contact> contacts = contactService.getAllContacts();
@@ -30,6 +36,12 @@ public class ContactController {
     }
 
     // GetMapping for localhost:8080/add-contact
+    /**
+     * Shows the new contact page.
+     * 
+     * @param model the model map
+     * @return the name of the contact.html template
+     */
     @GetMapping("/add-contact")
     public String showNewContactPage(ModelMap model) {
         // return to contact.html
@@ -37,6 +49,12 @@ public class ContactController {
     }
 
     // PostMapping for localhost:8080/add-contact
+    /**
+     * Adds a new contact to the system and redirects to the home page.
+     *
+     * @param contact the contact object containing the contact details
+     * @return a string representing the redirect URL
+     */
     @PostMapping("/add-contact")
     public String addNewContactPage(@ModelAttribute("contact") Contact contact) {
         contactService.addContact(contact.getFirstName(), contact.getLastName(), contact.getAddress(),
@@ -44,11 +62,13 @@ public class ContactController {
         return "redirect:/";
     }
 
-    // @RequestMapping("/list-contact")
-    // public List<Contact> listAllContacts(ModelMap model){
-    // List<Contact> contacts = contactService.getAllContacts();
-    // model.addAttribute("contacts", contacts);
-    // //return to list-contacts.html
-    // return contacts;
-    // }
+    @GetMapping("/update-contact")
+    public String showUpdateContactPage(@RequestParam int id, ModelMap model) {
+        Contact contact = contactService.findById(id);
+        model.addAttribute("contact", contact);
+
+        // return to contact.html
+        return "contact";
+    }
+
 }
