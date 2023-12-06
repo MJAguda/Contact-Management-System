08908616a3f65ae148a1787ea9803502b4ee3593
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-06 21:50:41 +0800

    Add and update contacts in ContactService

--- src/main/java/com/mjdminer/springboot/cms/services/ContactService.java
+++ src/main/java/com/mjdminer/springboot/cms/services/ContactService.java
@@ -2,6 +2,7 @@ package com.mjdminer.springboot.cms.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class ContactService {
         // Add contacts to ArrayList
         contacts.add(new Contact(++count, "John", "Doe", "123 Main St", "johndoe@gmail.com","+639397503598"));
         contacts.add(new Contact(++count, "Jane", "Doe", "124 Main St", "janedoe@gmail.com","+639999999999"));
-        contacts.add(new Contact(++count, "Mark Jayson", "Aguda", "125 Main St", "janedoe@gmail.com","+639999999999"));
+        contacts.add(new Contact(++count, "Mark Jayson", "Aguda", "125 Main St", "janedoe@gmail.com","+639999999779"));
     }
 
     // Method to getAllContacts
@@ -29,8 +30,24 @@ public class ContactService {
     }
 
     // Method to addContact
-    public void addContact(String firstName, String lastName, String address, String email, String contactNumber){
+    public void addContact(String firstName, String lastName, String address, String email, String contactNumber){        
         Contact contact = new Contact(++count, firstName, lastName, address, email, contactNumber);
+        contacts.add(contact);
+    }
+
+    private void deleteById(int id) {
+        Predicate<? super Contact> predicate = contact -> contact.getId() == id; 
+        contacts.removeIf(predicate);
+    }
+
+    public Contact findById(int id) {
+        Predicate<? super Contact> predicate = contact -> contact.getId() == id; 
+        Contact contact = contacts.stream().filter(predicate).findFirst().get();
+        return contact;
+    }
+
+    public void updateContact(Contact contact){
+        deleteById(contact.getId());
         contacts.add(contact);
     }
 }
