b43b43da700d43461dc74ac9c0f9fd1007e9b8a6
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-08 18:55:07 +0800

    Update button and details button functionality in scripts.js and update-contact.html

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,11 +1,45 @@
-document.getElementById('addButton').addEventListener('click', function() {
-    document.getElementById('pageTitle').innerText = 'Add Contact';
-});
-
-document.getElementById('updateButton').addEventListener('click', function() {
-    document.getElementById('pageTitle').innerText = 'Update Contact';
-});
-
-document.getElementById('detailsButton').addEventListener('click', function() {
-    document.getElementById('pageTitle').innerText = 'Details';
-});
+// Wait for the DOM to be ready
+$(document).ready(function () {
+        // Event handler for the Add button
+        $('#addButton').click(function () {
+            // Store a flag in localStorage
+            localStorage.setItem('isAddButtonClicked', 'true');
+        });
+    
+        // Check if the flag is set in localStorage
+        if (localStorage.getItem('isAddButtonClicked') === 'true') {
+            // Set the h1 text
+            $('h1').text('Add Contact');
+            // Clear the flag
+            localStorage.removeItem('isAddButtonClicked');
+        }
+    
+        // Event handler for the Update button
+        $('#updateButton').click(function () {
+                // Store a flag in localStorage
+                localStorage.setItem('isUpdateButtonClicked', 'true');
+            });
+        
+            // Check if the flag is set in localStorage
+            if (localStorage.getItem('isUpdateButtonClicked') === 'true') {
+                // Set the h1 text
+                $('h1').text('Update Contact');
+                // Clear the flag
+                localStorage.removeItem('isUpdateButtonClicked');
+            }
+    
+        // Event handler for the Details button
+        $('#detailsButton').click(function () {
+                // Store a flag in localStorage
+                localStorage.setItem('isDetailsButtonClicked', 'true');
+            });
+        
+            // Check if the flag is set in localStorage
+            if (localStorage.getItem('isDetailsButtonClicked') === 'true') {
+                // Set the h1 text
+                $('h1').text('Contact Details');
+                // Clear the flag
+                localStorage.removeItem('isDetailsButtonClicked');
+            }
+    });
+  
