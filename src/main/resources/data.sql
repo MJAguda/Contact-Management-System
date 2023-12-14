8f739b86e858efa90d3468b6818ab1b329e5f618
Miner <mark.jayson.aguda@gmail.com>
2023-12-14 12:39:16 +0800

    Update contact phone numbers in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,6 @@
 INSERT INTO contact (ID, FIRST_NAME, LAST_NAME, ADDRESS, EMAIL, CONTACT_NUMBER)
-VALUES (10001, 'John Cena', 'Doe', '123 Main St', 'john.doe@example.com', '+6391234567890'),
-    (10002, 'Jane', 'Smith', '456 Elm St', 'jane.smith@example.com', '+6399876543210'),
-    (10003, 'Michael', 'Johnson', '789 Oak St', 'michael.johnson@example.com', '+6395555555555'),
-    (10004, 'Sarah', 'Williams', '321 Pine St', 'sarah.williams@example.com', '+6391111111111'),
-    (10005, 'David', 'Brown', '654 Maple St', 'david.brown@example.com', '+6392222222222');
-  
+VALUES (10001, 'John Cena', 'Doe', '123 Main St', 'john.doe@example.com', '+639123456789'),
+       (10002, 'Jane', 'Smith', '456 Elm St', 'jane.smith@example.com', '+639987654321'),
+       (10003, 'Michael', 'Johnson', '789 Oak St', 'michael.johnson@example.com', '+639555555555'),
+       (10004, 'Sarah', 'Williams', '321 Pine St', 'sarah.williams@example.com', '+639111111111'),
+       (10005, 'David', 'Brown', '654 Maple St', 'david.brown@example.com', '+639222222222');
